683714d0a9765013e6f4356fc94c163e0795aad4
Oliver1334 <oliver.taylor95@outlook.com>
2023-11-25 22:15:07 +0000

    Dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import {CgMenuGridR, CgMenuGridO} from 'react-icons/cg'
 
 const Navbar = () => {
   return (
-    <div className='text-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text-3xl font-bold text-[#16a34a]'>REACT.</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-            <li>Github</li>
+        <ul className='flex hidden'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Projects</li>
+            <li className='p-4'>Contact</li>
+            <li className='p-4'>Github</li>
         </ul>
+        <div>
+            <CgMenuGridR />
+        </div>
     </div>
   )
 }
