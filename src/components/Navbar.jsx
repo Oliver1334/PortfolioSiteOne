4de56529de537254185ebf456522d678ceabf3f6
Oliver1334 <oliver.taylor95@outlook.com>
2023-11-25 21:59:26 +0000

    Navbar foundations

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#16a34a]'>REACT.</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+            <li>Github</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
