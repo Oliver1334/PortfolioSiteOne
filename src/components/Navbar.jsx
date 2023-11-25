27b9eb8014b5ebf2e4eae33aeece40cf87619911
Oliver1334 <oliver.taylor95@outlook.com>
2023-11-25 22:57:10 +0000

    Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {CgMenuGridR, CgMenuGridO} from 'react-icons/cg'
 
+
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
@@ -13,8 +14,20 @@ const Navbar = () => {
             <li className='p-4'>Github</li>
         </ul>
         <div>
-            <CgMenuGridR />
+            <CgMenuGridR size= "30"/>
         </div>
+        <div className= 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]'>
+        <h1 className='w-full text-3xl font-bold text-[#16a34a] m-4'>REACT.</h1>
+
+            <ul className='uppercase p-4'>
+            <li className='p-4 border-b border-gray-600'>Home</li>
+            <li className='p-4 border-b border-gray-600'>About</li>
+            <li className='p-4 border-b border-gray-600'>Projects</li>
+            <li className='p-4 border-b border-gray-600'>Contact</li>
+            <li className='p-4'>Github</li>
+            </ul>
+        </div>
+        
     </div>
   )
 }
