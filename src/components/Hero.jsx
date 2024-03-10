1bbd9c48438cfd0e775064c0b03511450edc9af4
Oliver1334 <oliver.taylor95@outlook.com>
2024-03-10 02:21:04 +0000

    react typed feature and button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React from "react";
+import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
-    <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-<p className='text-[#16a34a] font-bold p-2'>FULLSTACK SOFTWARE ENGINEER</p>
-<h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Oliver Taylor</h1>
-<div>
-    <p>Code on Brother!</p>
-</div>
-
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#16a34a] font-bold p-2">
+          FULLSTACK SOFTWARE ENGINEER
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Oliver Taylor
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Code on Brother!
+          </p>
+          <ReactTyped
+            className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+            strings={["Javascript", "ReactJS", "TailwindCSS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Learning to code through building projects.
+        </p>
+        <button className="bg-[#16a34a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-
-        </div>
-  )
-}
-
-export default Hero
+export default Hero;
