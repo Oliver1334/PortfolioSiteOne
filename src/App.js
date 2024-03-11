14a7597ac33f7c99be3fd34d3c08c56efe58349f
Oliver1334 <oliver.taylor95@outlook.com>
2024-03-11 01:25:29 +0000

    Featured project block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Feature from "./components/Feature";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Feature />
     </div>
   );
 }
