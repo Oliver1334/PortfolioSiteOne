2703aa587a659cb596dc7bde1b4d9451793d2e4f
Oliver1334 <oliver.taylor95@outlook.com>
2024-03-15 00:07:55 +0000

    Card styling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = () => {
     <div className='w-full py-[10rem] px-4 bg-white'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
             <div className='w-full border shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="" />
+                <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Single} alt="" />
                 <h2 className='text-2xl font-bold text-center py-8'>Project Card</h2>
                 <p className='text-center text-4xl font-bold' >Technology Used:</p>
                 <div className='text-center font-medium'>
@@ -32,7 +32,7 @@ const Cards = () => {
                 <button className='w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 bg-[#16a34a]' >Read More</button>
             </div>
             <div className='w-full border shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="" />
+                <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Single} alt="" />
                 <h2 className='text-2xl font-bold text-center py-8'>Project Card</h2>
                 <p className='text-center text-4xl font-bold' >Technology Used:</p>
                 <div className='text-center font-medium'>
